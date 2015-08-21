82e9d0cb20045d9446e14d151f617fc931ddd1ad
irwan adriansyah <adriansyah.irwan@gmail.com>
2015-08-21 17:45:37 +0700

    fixing

--- website/views/scripts/includes/footer_default.php
+++ website/views/scripts/includes/footer_default.php
@@ -64,6 +64,78 @@
       $(".submenuinstant").click(function (){
         $(".main").moveTo(4);
       });
+      $(".submenustrawberry").click(function (){
+        $(".main").moveTo(3);
+      });
+      $(".submenuchocoberry").click(function (){
+        $(".main").moveTo(4);
+      });
     });
    
   </script>
+<script>
+    $(document).foundation({
+  dropdown: {
+    // specify the class used for active dropdowns
+    active_class: 'opssen'
+  }
+});
+$( ".background1" ).hover(
+function() {
+$( this ).addClass( "hover" );
+$( ".background2" ).addClass( "hovering1");
+$( ".background3" ).addClass( "hovering1");
+//$( this ).append( $( "<span> ***</span>" ) );
+}, function() {
+$( this ).removeClass( "hover" );
+$( ".background2" ).removeClass( "hovering1");
+$( ".background3" ).removeClass( "hovering1");
+}
+);
+
+$( ".background2" ).hover(
+function() {
+$( this ).addClass( "hover" );
+$( ".background1" ).addClass( "hovering2");
+$( ".background3" ).addClass( "hovering2");
+//$( this ).append( $( "<span> ***</span>" ) );
+}, function() {
+$( this ).removeClass( "hover" );
+$( ".background1" ).removeClass( "hovering2");
+$( ".background3" ).removeClass( "hovering2");
+}
+);
+
+$( ".background3" ).hover(
+function() {
+$( this ).addClass( "hover" );
+$( ".background2" ).addClass( "hovering3");
+$( ".background4" ).addClass( "hovering3");
+//$( this ).append( $( "<span> ***</span>" ) );
+}, function() {
+$( this ).removeClass( "hover" );
+$( ".background2" ).removeClass( "hovering3");
+$( ".background4" ).removeClass( "hovering3");
+}
+);
+
+$( ".background4" ).hover(
+function() {
+$( this ).addClass( "hover" );
+$( ".background2" ).addClass( "hovering4");
+$( ".background3" ).addClass( "hovering4");
+//$( this ).append( $( "<span> ***</span>" ) );
+}, function() {
+$( this ).removeClass( "hover" );
+$( ".background2" ).removeClass( "hovering4");
+$( ".background3" ).removeClass( "hovering4");
+}
+);
+
+    
+  </script>
+  <script>
+    $(function(){
+        $('#menu').slicknav();
+    });
+</script>  
